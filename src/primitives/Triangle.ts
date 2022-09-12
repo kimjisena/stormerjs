bf60c053c22797980ee3f4e87a2b43fa5e52d139
Kim Jisena <pjohmaths@gmail.com>
2022-09-12 20:40:44 +0300

    feat: implement triangle drawing

--- src/primitives/Triangle.ts
+++ src/primitives/Triangle.ts
@@ -1,12 +1,36 @@
-import Point from "./Point";
+import Vector from "./Vector";
 import Shape from "./Shape";
 
 export default class Triangle extends Shape {
-  points: {p1: Point, p2: Point, p3: Point};
+  vectors: {vec1: Vector, vec2: Vector, vec3: Vector};
 
-  constructor (points: {p1: Point, p2: Point, p3: Point}) {
+  constructor (vectors: {vec1: Vector, vec2: Vector, vec3: Vector}) {
     super();
-    
-    this.points = points;
+    this.vectors = vectors;
+  }
+
+  render () {
+    const ctx = this.surface._surface;
+    // save Canvas state
+    ctx.save();
+
+    // apply config e.g. fillStyle
+    for (let [key, value] of this.config) {
+      ctx[key] = value;
+    }
+
+    // draw triangle
+    ctx.beginPath();
+    ctx.moveTo(this.vectors.vec1.x, this.vectors.vec1.y);
+    ctx.lineTo(this.vectors.vec2.x, this.vectors.vec2.y);
+    ctx.lineTo(this.vectors.vec3.x, this.vectors.vec3.y);
+    ctx.closePath();
+    ctx.fill();
+
+    // restore Canvas state
+    ctx.restore();
+
+    // reset status flage
+    this.status = false;
   }
 }
