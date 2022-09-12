360109489a5afd29b70c26da9a4eb93ca78c5743
Kim Jisena <pjohmaths@gmail.com>
2022-09-12 20:32:24 +0300

    feat: implement rectangle drawing

--- src/primitives/Rectangle.ts
+++ src/primitives/Rectangle.ts
@@ -1,16 +1,38 @@
-import Point from "./Point";
+import Vector from "./Vector";
 import Shape from "./Shape";
 
 export default class Rectangle extends Shape {
-  origin: Point;
+  origin: Vector;
   width: number;
   height: number;
 
-  constructor (origin: Point, width: number, height: number) {
+  constructor (origin: Vector, width: number, height: number) {
     super();
     
     this.origin = origin;
     this.width = width;
     this.height = height;
+  }
+
+  render () {
+    const ctx = this.surface._surface;
+    // save Canvas state
+    ctx.save();
+
+    // apply config e.g. fillStyle
+    for (let [key, value] of this.config) {
+      ctx[key] = value;
+    }
+
+    // draw rectangle
+    ctx.rect(this.origin.x, this.origin.y, this.width, this.height)
+    ctx.fill();
+
+
+    // restore Canvas state
+    ctx.restore();
+
+    // reset status flage
+    this.status = false;
   }
 }
